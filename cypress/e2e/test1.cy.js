90f5c226854b16a704a86203b394f4835a7e6bba
Magdalena Kilianska <vinverse91@gmail.com>
2024-01-18 20:30:36 +0000

    adding new tests

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,23 @@
+describe('Visits shopping app page and tests the elements', () => {
+  it('successfully loads', () => {
+    cy.visit('../../index.html') 
+
+    // Checking if the 'add' button exists
+    // Checking if an item in the list is ticked off
+    // Checking if the delete button removes an item from the list
+  
+    cy.contains("button", "add").should("be.visible").click();
+
+    cy.get("[input-cy=inputBox]").type("bananas");
+    cy.get("[button-cy=addButton]").click();
+    cy.get("#list-container").should("contain", "bananas");
+
+    cy.get("li").click();
+    cy.get('[class=checked]').should("be.visible")
+
+    cy.get("span").click();
+    cy.get("#list-container").should("contain", "");
+  });
+})
+
+  
